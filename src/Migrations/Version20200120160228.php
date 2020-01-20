cb3676a12aa9567c5fa5861275919881f6cefa66
Egor Ushakov <eushakov@gmail.com>
2020-01-20 19:37:31 +0300

    Strip old migrations

--- src/Migrations/Version20200120160228.php
+++ src/Migrations/Version20200120160228.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200120092021 extends AbstractMigration
+final class Version20200120160228 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,7 @@ final class Version20200120092021 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE exchange_rate CHANGE quote quote NUMERIC(16, 8) NOT NULL');
+        $this->addSql('CREATE TABLE exchange_rates (id INT AUTO_INCREMENT NOT NULL, date DATE NOT NULL COMMENT \'(DC2Type:date_immutable)\', base_currency VARCHAR(3) NOT NULL, quote_currency VARCHAR(3) NOT NULL, quote NUMERIC(16, 8) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +30,6 @@ final class Version20200120092021 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE exchange_rate CHANGE quote quote NUMERIC(12, 8) NOT NULL');
+        $this->addSql('DROP TABLE exchange_rates');
     }
 }
