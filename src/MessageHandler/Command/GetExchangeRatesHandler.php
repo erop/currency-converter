c1d8e7b27fc40ea864e1de04a0dfab3c4ed5111e
Egor Ushakov <eushakov@gmail.com>
2021-02-24 12:53:12 +0300

    Translate README into English

--- src/MessageHandler/Command/GetExchangeRatesHandler.php
+++ src/MessageHandler/Command/GetExchangeRatesHandler.php
@@ -2,7 +2,6 @@
 
 namespace App\MessageHandler\Command;
 
-use App\Entity\ExchangeRate;
 use App\Message\Command\PopulateExchangeRates;
 use App\Repository\ExchangeRateRepository;
 use App\Service\RateService;
